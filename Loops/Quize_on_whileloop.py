c2bdc4a1a06fdbae9d30a182e60940923955e9bd
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-02 17:46:49 +0530

    Added Loops

--- Loops/Quize_on_whileloop.py
+++ Loops/Quize_on_whileloop.py
@@ -10,4 +10,13 @@ while i<=50:
 i = 0
 while i<5:
     print("Gaythri")
-    i+=1
+    i+=1# we can write i = i+1 also
+
+#write a program to print content of list using while loop 
+
+l = ["Gaythri", "John", "Alice", "Bob", "Eve"]
+i = 0
+
+while i < len(l):
+    print(l[i])
+    i += 1  # Ha step khup mahatvacha aahe, yaamule i pudhe jato ani loop thambto
