c833d773bf835ea75f879209d9d1d8253b25d383
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-02 17:30:19 +0530

    Added Loops

---
+++ Loops/While_loop.py
@@ -0,0 +1,18 @@
+i = 1
+while i<=51:
+    print(i)
+    i+=1
+
+#write a program to print 1 to 50 using while loop 
+
+i = 1
+while i<=50:
+    print(i)
+    i+=1
+
+#write a program to print list of content 
+
+i = 0
+while i<5:
+    print("Gaythri")
+    i+=1
