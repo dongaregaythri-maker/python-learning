9aec4cba61a4485189ca257f94720fd026b806d4
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-02 18:59:35 +0530

    Added Loops

---
+++ Loops/Break_and_continue_inloops.py
@@ -0,0 +1,11 @@
+#break statement
+for i in range(100):
+    if i == 10:
+        break #break the loop when 10 is found
+    print(i)
+
+#Continue statement 
+for i in range (100):
+    if i ==10:
+        continue #it will skip the iteration when i is 10 and continue with the next iteration
+    print(i)
