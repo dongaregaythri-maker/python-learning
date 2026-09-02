fbcc4f5e77e3e3dc6df883bda58eab905dceea1b
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-02 10:49:46 +0530

    Added loops.py file

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+#write a program to create dictionary of hindi words with values as their english traslation . provide user with an option to look it up 
+words = {
+    "madat": "help",
+    "pyaar": "love",
+    "dost": "friend",
+    "ghar": "home",
+    "khushi": "happiness",
+    "shanti": "peace",}
+
+word = input("Enter a word you want meaning of: ")
+print(words[word])
