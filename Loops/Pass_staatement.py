9a31624b3bb2b148e681db0441441c700faa6ab2
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-02 19:04:33 +0530

    Added Loops

---
+++ Loops/Pass_staatement.py
@@ -0,0 +1,7 @@
+#when you want to stop the current task and do nothing and move to next task you can use pass statement
+for i in range(100):
+    pass
+for i in range (10):
+    print(i)
+    i += 1
+    
