9ebaee4b435df51f21370e979b60cf1c9aa5f012
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-09 15:20:06 +0530

    Added Functions

--- Functions/functions.py
+++
@@ -1,8 +0,0 @@
-def avg(): #function defination 
-    a=int(input("Enter the your number: "))
-    b=int(input("Enter the your number: "))
-    c=int(input("Enter the your number: "))
-    average=(a+b+c)/3
-    print(average)
-
-avg()
