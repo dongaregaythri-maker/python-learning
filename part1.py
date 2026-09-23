f107ae955053308225d52479f1e8d0f41e5f2382
Gaythri Dongare <dongaregaythri@gmail.com>
2026-09-23 17:25:10 +0530

    practiceset

---
+++ part1.py
@@ -0,0 +1,36 @@
+# question on :Print function , Datatype , Variable 
+#q1 add a person with first name as Tony and last name as Stark 
+#Tony age is 53
+#Tonys hight is 1.85m
+ #take his superhero name as input and print it 
+# name = "Tony"
+# last_name = "Stark"
+# tonys_age = 53
+# tonys_height = 1.85
+
+# superhero_name = input("Tony's superhero name: ")
+
+# print(name)
+# print(last_name)
+# print(tonys_age)
+# print(tonys_height)
+# print("Superhero Name:", superhero_name)
+
+# Create a Python program for a student:
+
+# First name: Rahul
+# Last name: Sharma
+# Age: 21
+# Height: 1.75 m
+# Take the student's favorite subject as input
+# Print all the details.
+First_name =("Rahul")
+Last_name=("Sharma")
+Age = (21)
+Height= (1.75)
+print(First_name)
+print(Last_name)
+print(Age)
+print(Height)
+favorite_subject = input("Enter your favorite subject : ")
+
